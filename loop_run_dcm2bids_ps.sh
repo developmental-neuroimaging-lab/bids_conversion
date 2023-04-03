fd696aae3edb8278d4f275df60622099732c4e12
Meaghan Perdue <meaghan@lyra.healthy.bewell.ca>
2023-04-03 14:09:46 -0600

    updated paths and loop

--- loop_run_dcm2bids_ps.sh
+++ loop_run_dcm2bids_ps.sh
@@ -2,9 +2,11 @@
 # if running dcm2bids in a conda environment, make sure to activate the environment first, e.g. conda activate dcm2bids
 #ps_subs.txt is a text file containing 2 columns: 
 # col1=subject ID (no "sub-")
-# col2=session ID (with "ses-")
+# col2=session ID (no "ses-")
 # script by Meaghan Perdue, April 2023
 
-for i in $(cat ps_subs.txt); do
-    sh run_dcm2bids_ps ${i}
-    done
+subjects=ps_subs.txt
+
+while read i; do
+    sh run_dcm2bids_ps.sh ${i}
+    done <"$subjects"
--- run_dcm2bids_ps.sh
+++ run_dcm2bids_ps.sh
@@ -1,6 +1,13 @@
+# run dicom2bids for 1 subject/session
+# if running dcm2bids in a conda environment, make sure to activate the environment first, e.g. conda activate dcm2bids
+# run by run_dcm2bids_ps.sh subject sessionnumber
+# or use loop dcm2bids script over a batch of subjects using loop_run_dcm2bids_ps.sh
+# change directories and config files to use for other studies
+# script by Meaghan Perdue, April 2023
+
 export dicom_dir=/Volumes/catherine_team/DataSortedForBIDS/PS-Sorted
 export bids_dir=/Volumes/catherine_team/Project_Folders/Preschool/preschool_bids
-export config=/Volumes/catherine_team/DataSortedForBIDS/preschool_config.json
+export config=/Volumes/catherine_team/DataSortedForBIDS/bids_conversion/preschool_config.json
 
 dcm2bids --forceDcm2niix --clobber \
  -d $dicom_dir/${1}/ses-${2} \
